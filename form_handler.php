29f0e42370e9f57bc263862828071b67bb285c30
BARATH-LAKSHMAN <barathlakshman06@.com>
2022-01-13 06:24:37 -0800

    first commit

---
+++ form_handler.php
@@ -0,0 +1,27 @@
+<?php
+$name = $_POST["name"];
+$visitor_email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+$email_from = 'info@btfesports.com';
+
+$email_subject = 'New Query Submission';
+
+$email_body = "User Name: $name/n".
+              "User Email: $visitor_email/n".
+              "Subject: $subject/n".
+              "Message: $message/n";
+
+$to = 'barathlakshman06@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-T0: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: index3.html")
+
+
+?>
---
+++ form_handler_reg.php
@@ -0,0 +1,31 @@
+<?php
+$name = $_POST["name"];
+$ig_name = $_POST["ig_name"];
+$id = $_POST["id"];
+$visitor_email = $_POST["email"];
+$number = $_POST["number"];
+$category = $_POST["dropdown"];
+
+$email_from = 'info@btfesports.com';
+
+$email_subject = 'New Query Submission';
+
+$email_body = "User Name: $name/n".
+              "In Game Name: $ig_name".
+              "In Game ID: $id/n".
+              "Subject: $number/n".
+              "Message: $category/n".
+              "User Email: $visitor_email/n";
+
+$to = 'barathlakshman06@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-T0: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: index3.html")
+
+
+?>
