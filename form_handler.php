0ebb3f8a42ff4c450947850aac850d4317a29444
BARATH-LAKSHMAN <96363307+BARATH-LAKSHMAN@users.noreply.github.com>
2022-01-13 08:23:42 -0800

    Update form_handler.php

--- form_handler.php
+++ form_handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST["email"];
 $subject = $_POST["subject"];
 $message = $_POST["message"];
 
-$email_from = 'info@btfesports.com';
+$email_from = 'info@btfesports.pages.dev';
 
 $email_subject = 'New Query Submission';
 
